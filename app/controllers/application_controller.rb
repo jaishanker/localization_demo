d997162d09014531b8ebc3030b52dd197ce94da5
AkitaOnRails <fabioakita@gmail.com>
2012-07-13 17:43:41 -0300

    Adding rails-translate-routes support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_locale
+  before_filter :set_locale_from_url
+
+  private
+
+  def set_locale
+    lang = request.env['HTTP_ACCEPT_LANGUAGE']
+    lang = lang[/^[a-z]{2}/]
+    lang = :"pt-BR" if lang == "pt"
+    I18n.locale = params[:locale] || lang
+  end
 end
