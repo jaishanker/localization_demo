ea8f61e2c6da7071953ea0c670892947d883cdaa
AkitaOnRails <fabioakita@gmail.com>
2012-07-14 17:24:42 -0300

    Moving footer buttons to proper top navigational menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,17 @@ module ApplicationHelper
     end
     links.join("\n").html_safe
   end
+
+  def navigation_links
+    links = []
+    options = params[:controller] == "welcome" ? { class: "active" } : {}
+    links << content_tag(:li, link_to(t("hello"), welcome_path), options).html_safe
+
+    options = params[:controller] == "articles" ? { class: "active" } : {}
+    links << content_tag(:li, link_to(t("articles.title"), articles_path), options).html_safe
+
+    links << content_tag(:li, link_to(t("admin.title"), admin_dashboard_path)).html_safe
+
+    content_tag(:ul, links.join("\n").html_safe, class: "nav")
+  end
 end
