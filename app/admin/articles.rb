77f498a237f68032a8a71a63ff2669756164ed49
AkitaOnRails <fabioakita@gmail.com>
2012-07-14 17:24:23 -0300

    Customizing the show admin view

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -7,6 +7,22 @@ ActiveAdmin.register Article do
     default_actions
   end
 
+  show do |article|
+    attributes_table do
+      row :slug
+      I18n.available_locales.each do |locale|
+        h3 I18n.t(locale, scope: ["translation"])
+        div do
+          h4 article.translations.where(locale: locale).first.title
+        end
+        div do
+          article.translations.where(locale: locale).first.body_html.html_safe
+        end
+      end
+    end
+    active_admin_comments
+  end
+
   form do |f|
     f.input :slug
     f.globalize_inputs :translations do |lf|
