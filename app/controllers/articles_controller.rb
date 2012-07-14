53e6299e60b05f9d9cc350ce3bb994c698459646
AkitaOnRails <fabioakita@gmail.com>
2012-07-14 13:10:59 -0300

    Adding locale sentitive links to change locale

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
-    @body_html = RDiscount.new(@article.body || "").to_html
+    @body_html = RDiscount.new(@article.body || "").to_html.html_safe
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def language_links
+    links = []
+    I18n.available_locales.each do |locale|
+      locale_key = "translation.#{locale}"
+      if locale == I18n.locale
+        links << I18n.t(locale_key)
+      else
+        links << link_to(I18n.t(locale_key), url_for(locale: locale.to_s))
+      end
+    end
+    content_tag(:p, links.join(" | ").html_safe)
+  end
 end
